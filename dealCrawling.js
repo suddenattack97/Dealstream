99718eb4a1bbe4bd8863ab0ebe3755f12c923a57
Kang Minsu <sudden_attack@naver.com>
2023-11-30 09:55:40 +0900

    이전 데이터랑 비교해서 차집합 아이템 구한 후 hist 에 저장하도록 수정

--- dealCrawling.js
+++ dealCrawling.js
@@ -17,6 +17,12 @@ function getKSTDate() {
     return now.toISOString().replace('T', ' ').substring(0, 19); // 'YYYY-MM-DD HH:mm:ss' 형식으로 변환합니다.
 }
 
+let previousItems = []; // 이전 아이템을 저장할 전역 변수
+
+function calculateDifference(newItems, oldItems) {
+    const newItemCodes = new Set(newItems.map(item => item.goodsCode));
+    return oldItems.filter(item => !newItemCodes.has(item.goodsCode));
+}
 async function fetchItems(url) {
     try {
         const response = await axios.get(url);
@@ -28,31 +34,43 @@ async function fetchItems(url) {
         $('.inner').each((index, element) => {
             const title = $(element).find('.title').text().trim();
             const salePercentage = $(element).find('.sale strong').text();
-            // const originalPrice = $(element).find('.price del').text().trim();
-            // const salePrice = $(element).find('.price strong').text().trim();
-            // const salePercentage = $(element).find('.sale strong').text().trim() || '0';
-            // const salePercentageInt = parseInt(salePercentage, 10); // 문자열을 정수로 변환
-        
-            // const originalPrice = $(element).find('.price del').text().trim().replace(/[^\d]/g, '');
-            // const salePrice = $(element).find('.price strong').text().trim().replace(/[^\d]/g, '');
+
             let originalPrice = $(element).find('.price del').text().trim().replace(/[^\d]/g, '');
             let salePrice = $(element).find('.price strong').text().trim().replace(/[^\d]/g, '');
             originalPrice = originalPrice ? parseInt(originalPrice, 10) : 0;
             salePrice = salePrice ? parseInt(salePrice, 10) : 0;
 
             const imageUrl = $(element).find('img.thumb').attr('src');
-            const goodsCodeTmp = $(element).find('.inner a').attr('href');
+            const goodsCodeTmp = $(element).find('.inner > a').attr('href');
             const goodsCode = goodsCodeTmp.split('goodscode=')[1];
 
             // 상품 정보를 객체에 추가합니다.
             items.push({ title, salePercentage, originalPrice, salePrice, imageUrl, goodsCodeTmp, goodsCode });
         });
+        // 새로 크롤링한 데이터와 이전 데이터를 비교
+        const excludedItems = calculateDifference(items, previousItems);
+        if (excludedItems.length > 0) {
+            console.log('제외된 아이템 발생:', excludedItems);
+            insertIntoItemHist(excludedItems);
+        }
+        // 현재 데이터를 이전 데이터로 저장
+        previousItems = items;
 
         return items;
     } catch (error) {
         console.error(`Error fetching items: ${error}`);
         return [];
     }
+}
+function insertIntoItemHist(excludedItems) {
+    excludedItems.forEach(item => {
+        const query = 'INSERT INTO item_hist (itemdate, itemcode, title, percentage, originalprice, saleprice, imageurl,shop_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [getKSTDate(), item.goodsCode, item.title, item.salePercentage, item.originalPrice, item.salePrice, item.imageUrl, 'gmarket'];
+
+        connection.query(query, values, (insertError) => {
+            if (insertError) throw insertError;
+        });
+    });
 }
 function saveToDatabase(items) {
     // 테이블 초기화
@@ -61,8 +79,8 @@ function saveToDatabase(items) {
 
         // 각 아이템을 데이터베이스에 삽입
         items.forEach(item => {
-            const query = 'INSERT INTO realtime (itemdate, itemcode, title, percentage, originalprice, saleprice, imageurl) VALUES (?, ?, ?, ?, ?, ?, ?)';
-            const values = [getKSTDate(), item.goodsCode, item.title, item.salePercentage, item.originalPrice, item.salePrice, item.imageUrl];
+            const query = 'INSERT INTO realtime (itemdate, itemcode, title, percentage, originalprice, saleprice, imageurl,shop_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+            const values = [getKSTDate(), item.goodsCode, item.title, item.salePercentage, item.originalPrice, item.salePrice, item.imageUrl, 'gmarket'];
 
             connection.query(query, values, (insertError) => {
                 if (insertError) throw insertError;
@@ -79,4 +97,4 @@ setInterval(() => {
     fetchItems(url)
         .then(items => saveToDatabase(items))
         .catch(error => console.error(error));
-}, 5000);
+}, 60000);
